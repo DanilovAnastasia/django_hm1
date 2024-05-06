ba91ec4a3336dc6ca36c174891f52e3e989cd520
DanilovAnastasia <grey-mice@mail.ru>
2024-05-06 22:57:35 +0300

    update homework number 6. forms

--- catalog/models.py
+++ catalog/models.py
@@ -25,6 +25,7 @@ class Product(models.Model):
     created_at = models.DateTimeField(verbose_name='дата создания', auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name='дата последнего изменения', auto_now=True)
     category = models.ForeignKey('Category', on_delete=models.PROTECT)
+    views_count = models.IntegerField(default=0, verbose_name='количество просмотров')
 
     def __str__(self):
         return f'{self.name} ({self.category})'
@@ -33,4 +34,20 @@ class Product(models.Model):
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
 
-# Create your models here.
+
+class Version(models.Model):
+    """Модель для версии продукта"""
+    # related_name говорит об отношении один ко многим (у одного продукта м.б. несколько версий).
+    # М.Б. обращаться как product.versions, а не product.version_set
+    product = models.ForeignKey(Product, related_name='versions', verbose_name='Продукт', on_delete=models.CASCADE)
+    number = models.PositiveIntegerField(verbose_name=' Номер версии', blank=True, null=True)
+    name = models.CharField(max_length=150, verbose_name='Название версии')
+    is_current = models.BooleanField(default=True, verbose_name='Признак актуальности')
+
+    class Meta:
+        verbose_name = 'Версия'
+        verbose_name_plural = 'Версии'
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
--- catalog/urls.py
+++ catalog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from catalog.apps import CatalogConfig
 # from catalog.views import home, contacts, product
-from catalog.views import ProductListView, ProductDetailView, ContactsPageView
+from catalog.views import ProductListView, ProductDetailView, ContactsPageView, ProductCreateView, ProductUpdateView, ProductDeleteView
 
 app_name = CatalogConfig.name
 
@@ -10,7 +10,13 @@ urlpatterns = [
     # path('', home, name='home'),
     # path('contacts/', contacts, name='contacts'),
     # path('product/<int:pk>', product, name='product')
-    path('', ProductListView.as_view(), name='home'),
+    path('', ProductListView.as_view(), name='products_list'),
     path('contacts/', ContactsPageView.as_view(), name='contacts'),
-    path('product/<int:pk>', ProductDetailView.as_view(), name='product')
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    # Путь для вывода страницы при создании нового объекта
+    path('product/', ProductCreateView.as_view(), name='create'),
+    # Путь для вывода страницы редактирования продукта
+    path('product/<int:pk>/update/', ProductUpdateView.as_view(), name='update'),
+    # Путь для вывода страницы c удалением продукта
+    path('product/<int:pk>/delete/', ProductDeleteView.as_view(), name='delete')
 ]
